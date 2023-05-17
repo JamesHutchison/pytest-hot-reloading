cf99439127535dd94ce7f6ba202261395edfa97c
James Hutchison <122519877+JamesHutchison@users.noreply.github.com>
2023-05-17 12:38:30 -0700

    #19 add CI and update minimum version to Python 3.10 (#21)
    * Remove poetry init stuff from postCreateCommand
    * Relax required versions
    * Add CI
    * Update version to alpha.3

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -10,12 +10,6 @@ sudo chown vscode .venv || true
 
 # make the python binary location predictable
 poetry config virtualenvs.in-project true
-poetry init -n --python=~3.11 || true
-# mypy - static type checking
-# ruff - fast flake8 and isort alternative
-# black - opinionated formatting
-# pytest - testing
-poetry add mypy ruff black pytest --group=dev || true
 poetry install --with=dev || true
 
 mkdir -p .dev_container_logs
