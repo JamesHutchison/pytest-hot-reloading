32ef21e382eb030fb65db278866ecae976aa9446
James Hutchison <jamesghutchison@proton.me>
2023-06-15 22:46:23 +0000

    Start postgres server on dev container start

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # run in the background at startup
-nohup bash .devcontainer/postStartBackground.sh > ".dev_container_logs/postStartBackground.out" &
+nohup "bash -c .devcontainer/postStartBackground.sh & > .dev_container_logs/postStartBackground.out"
 
-sleep 1
+docker compose -f tests/workarounds/pytest_django/docker-compose.yml up -d postgres
