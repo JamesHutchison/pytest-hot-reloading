da1722f30c160a4202c90f747aa5b06fa0f2a5ec
James Hutchison <jamesghutchison@proton.me>
2023-06-15 22:51:24 +0000

    Add support for unregistering workarounds from libraries Update tests

--- pytest_hot_reloading/workarounds.py
+++ pytest_hot_reloading/workarounds.py
@@ -1,43 +1,44 @@
-from typing import Callable, Generator, Optional
+from typing import Callable, Generator, NamedTuple, Optional
 
-workarounds: list[Callable[[], Optional[Generator]]] = []
+Workaround = NamedTuple(
+    "Workaround", [("module", str), ("func", Callable[[], Optional[Generator]])]
+)
 
-
-def register_workaround(func: Callable[[], Optional[Generator]]) -> None:
-    """
-    Register a workaround. A workaround is a function that takes in
-    a list of the arguments passed into pytest. The function may
-    be a generator function, and if so, yield will separate the pre
-    and post calls.
-    """
-    workarounds.append(func)
+workarounds: list[Workaround] = []
 
 
-@register_workaround
+def register_workaround(module_name: str):
+    def _register_workaround(func: Callable[[], Optional[Generator]]) -> None:
+        """
+        Register a workaround. A workaround is a function that takes in
+        a list of the arguments passed into pytest. The function may
+        be a generator function, and if so, yield will separate the pre
+        and post calls.
+        """
+        workarounds.append(Workaround(module_name, func))
+
+    return _register_workaround
+
+
+@register_workaround("xdist")
 def xdist_workaround() -> None:
     """
     pytest-xdist is not supported. The test collection behaves differently
     and some libraries such as pytest-django may have bugs when its enabled.
     """
-    try:
-        from xdist import plugin  # type: ignore
-    except ImportError:
-        return  # not installed
+    from xdist import plugin  # type: ignore
 
     # monkey patch to force zero processes
     plugin.parse_numprocesses = lambda s: None
 
 
-@register_workaround
+@register_workaround("pytest_django")
 def pytest_django_tox_workaround() -> None:
     """
     pytest-django will attempt to add a suffix and they will accumulate with each run.
     Note that running tox with hot reloading doesn't make sense anyways.
     """
-    try:
-        from pytest_django import fixtures  # type: ignore
-    except ImportError:
-        return  # not installed
+    from pytest_django import fixtures
 
     # monkey patch to disable suffix logic used by xdist and tox
     fixtures._set_suffix_to_test_databases = lambda suffix: None
@@ -45,7 +46,17 @@ def pytest_django_tox_workaround() -> None:
 
 def run_workarounds_pre() -> list[Generator]:
     in_progress_workarounds = []
-    for workaround in workarounds:
+    for module_name, workaround in workarounds:
+        try:
+            __import__(module_name)
+        except ImportError:
+            continue  # not installed
+        try:
+            __import__(f"{module_name}._clear_hot_reload_workarounds")
+        except ImportError:
+            pass
+        else:
+            continue  # workaround no longer needed
         result = workaround()
         if result is not None:
             next(result)
---
+++ tests/test_workarounds.py
@@ -0,0 +1,76 @@
+from typing import Callable
+
+import pytest  # type: ignore
+from megamock import MegaPatch
+
+from pytest_hot_reloading import workarounds
+
+
+@pytest.fixture(autouse=True)
+def clear_workarounds() -> None:
+    new_workarounds: list[Callable] = []
+    MegaPatch.it(workarounds.workarounds, new=new_workarounds)
+
+
+def test_single_shot_workaround() -> None:
+    was_called = False
+
+    @workarounds.register_workaround("tests.test_workarounds")
+    def my_workaround():
+        nonlocal was_called
+
+        was_called = True
+
+    in_progress = workarounds.run_workarounds_pre()
+    assert len(in_progress) == 0
+    workarounds.run_workarounds_post(in_progress)
+
+    assert was_called
+
+
+def test_multi_part_workaround() -> None:
+    status = 0
+
+    @workarounds.register_workaround("tests.test_workarounds")
+    def my_workaround():
+        nonlocal status
+
+        status += 1
+        yield
+        status += 1
+
+    in_progress = workarounds.run_workarounds_pre()
+    assert len(in_progress) == 1
+    workarounds.run_workarounds_post(in_progress)
+
+    assert status == 2
+
+
+def test_not_installed_workaround() -> None:
+    was_called = False
+
+    @workarounds.register_workaround("this.doesnt.exist")
+    def my_workaround():
+        nonlocal was_called
+
+        was_called = True
+
+    in_progress = workarounds.run_workarounds_pre()
+    assert len(in_progress) == 0
+    workarounds.run_workarounds_post(in_progress)
+
+    assert was_called is False
+
+
+def test_unregistered_workaround() -> None:
+    was_called = False
+
+    @workarounds.register_workaround("tests.workarounds.unregistered_workaround")
+    def my_workaround():
+        nonlocal was_called
+
+        was_called = True
+
+    workarounds.run_workarounds_pre()
+
+    assert was_called is False
